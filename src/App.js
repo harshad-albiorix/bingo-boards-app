4b1e5001cb7157ca4972a7a25bdf6a41e1e642e9
harshad-albiorix <harshadr.albiorix@gmail.com>
2024-06-04 17:25:32 +0530

    Implemented Bingo Board task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { BingoBoard } from "./pages";
+
+function App() {
+  return (
+    <div className="w-full main-container">
+      <BingoBoard />
+    </div>
+  );
+}
+
+export default App;
