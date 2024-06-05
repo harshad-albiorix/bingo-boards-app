8adbc43120de00b436ea5d6f6be202f6d9665cef
harshad-albiorix <harshadr.albiorix@gmail.com>
2024-06-05 12:40:15 +0530

    [Fixed] Interval time.

--- src/pages/BingoBoard/index.jsx
+++ src/pages/BingoBoard/index.jsx
@@ -40,7 +40,7 @@ export const BingoBoard = () => {
       const n = arr[random];
       arr.splice(random, 1);
       setNum(n);
-      await new Promise((resolve) => setTimeout(resolve, 100));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
   }, [start]);
 
